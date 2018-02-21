cb5f12dc2a0f51a736dac0499bfe21cb3ff66a33
ahuscia <a.huscia@gmail.com>
2018-02-21 15:22:05 +0100

    Changes in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-$(function(){
-    var carouselList = $("#carousel ul");
+$(function () {
+	var carouselList = $("#carousel ul");
 
-    setInterval(changeSlide, 3000);
+	setInterval(changeSlide, 3000);
 
-    function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	function changeSlide() {
+		carouselList.animate({ 'marginLeft': -400 }, 500, moveFirstSlide);
 	}
 
-	function moveFirstSlide(){
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
 
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft: 0});
-    }
+		lastItem.after(firstItem);
+		carouselList.css({ marginLeft: 0 });
+	}
 });
